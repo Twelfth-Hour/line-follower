097931d2c8d277315f2ed807bc26c17816085075
Sahil <sahilbondre@gmail.com>
2020-02-07 11:55:38 +0530

    add test for input pins

--- test/digital-input.test/digital-input.test.ino
+++ test/digital-input.test/digital-input.test.ino
@@ -2,14 +2,14 @@ int inPinsCount = 8;
 int inPins[8] = {2, 3, 4, 5, 6, 7, 8, 12};
 void setup() {
   Serial.begin(9600);
-  for(int i = 0; i < inPinsCount; ++i) {
+  for (int i = 0; i < inPinsCount; ++i) {
     pinMode(inPins[i], INPUT);
   }
 }
 
 void loop() {
   Serial.print("CLEAR\n");
-  for(int i = 0; i < inPinsCount; ++i) {
+  for (int i = 0; i < inPinsCount; ++i) {
     if (digitalRead(inPins[i]) == HIGH) {
       Serial.write("PIN HIGH: ");
       Serial.print(inPins[i]);
